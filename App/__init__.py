0f1db1d52b7b84feb89225769823f3cbad48af7e
Arun Karuppiah <10533185@mydbs.ie>
2020-03-03 14:31:34 +0000

    code modified

--- App/__init__.py
+++ App/__init__.py
@@ -2,12 +2,13 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
     )
 
     if test_config is None:
@@ -23,25 +24,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    
-
-    from . import db
-    db.init_app(app)
-    
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    from . import blog
-    app.register_blueprint(blog.bp)
-       
-       
-
-    from . import kart
-    app.register_blueprint(kart.bp)
-   
-
-    from . import products
-    app.register_blueprint(products.bp)
-    app.add_url_rule('/', endpoint='index')
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
     return app
